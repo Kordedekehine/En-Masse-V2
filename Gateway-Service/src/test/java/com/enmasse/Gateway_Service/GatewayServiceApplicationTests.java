b039955552e6b803faa511949abc5f3092e9da91
Salami Korede <salami.korede@prunny.com>
2025-04-02 16:15:43 +0100

    set up

---
+++ Gateway-Service/src/test/java/com/enmasse/Gateway_Service/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enmasse.Gateway_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Logistic-Service/src/test/java/com/enmasse/Logistic_Service/LogisticServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enmasse.Logistic_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
